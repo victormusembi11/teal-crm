c21a9a5679dd9f22e8d5f163832fb50cecb39ba1
victormusembi11 <victor.musembi11@gmail.com>
2024-02-17 11:54:04 +0300

    added team model and get active team method

--- accounts/models.py
+++ accounts/models.py
@@ -3,11 +3,26 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from team.models import Team
+
 
 class UserProfile(models.Model):
     """User profile model."""
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    active_team = models.ForeignKey(
+        Team,
+        related_name="userprofiles",
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+    )
+
+    def get_active_team(self):
+        if self.active_team:
+            return self.active_team
+        else:
+            return Team.objects.filter(members__in=[self.user.id]).first()
 
     def __str__(self) -> str:
         """Model str representation."""
