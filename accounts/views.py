a420917f06e119050be14f11ebf43b607e6f21bb
victormusembi11 <victor.musembi11@gmail.com>
2024-02-17 11:31:50 +0300

    login, signup and logout pages

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+"""Accounts views."""
 
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
+
+from accounts.models import UserProfile
+
+
+def signup(request):
+    """User create form view."""
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            # create a profile for the user
+            UserProfile.objects.create(user=user)
+
+            return redirect("/")  # TODO: redirect to the login page
+
+    form = UserCreationForm()
+
+    return render(request, "accounts/signup.html", {"form": form})
